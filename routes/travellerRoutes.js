c17fc0893039db535f94031e663f3532358992b4
rajivsuting <rajivsuting16@gmail.com>
2024-10-16 01:17:41 +0530

    updated search functionality

--- routes/travellerRoutes.js
+++ routes/travellerRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const travellerController = require("../controllers/travellerController");
+const { protect, restrictTo } = require("../middleware/authMiddleware");
 
 // Create a new Traveller
 router.post("/create", travellerController.createTraveller);
 
 // Get all Travellers with Pagination and Search
-router.get("/all", travellerController.getTravellers);
+router.get("/all", protect, travellerController.getTravellers);
 
 // Get a Traveller by ID
 router.get("/:id", travellerController.getTravellerById);
